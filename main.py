4b648b4701e4e096f38dbfed702e1497ff334159
Guy <3strategy@gmail.com>
2021-08-18 21:11:47 +0300

    a different way of writing the blink.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from machine import Pin, Timer
+from machine import Pin
+import time
 
-led = Pin(25, Pin.OUT)
-tim = Timer()
-
-
-def tick(timer):
-    global led
-    led.toggle()
+led = Pin(25, Pin.OUT)  # similar setting of pin 25 to OUTPUT
+                        # note that such a function returns an object, but also modifies this object.
+                        # this is more Object Oriented than the comparable Arduino blink native code
 
 
-tim.init(freq=2.5, mode=Timer.PERIODIC, callback=tick)
+# another example of blinking the internal led.
+# previous code was from https://github.com/raspberrypi/pico-micropython-examples/blob/master/blink/blink.py#L1-L9
+# this version is from https://themachineshop.uk/getting-started-with-the-pi-pico-and-pycharm/
+
+while True:
+    led(1)
+    time.sleep(0.5)  # this sleep code better resembles the more traditional MCU c code
+    led(0)
+    time.sleep(0.1)
