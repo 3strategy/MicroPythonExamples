a165b4008ceeb3a3169a4bd9473b3c32658183f9
Guy <3strategy@gmail.com>
2021-08-18 22:06:12 +0300

    servo operation example.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from machine import Pin
-import time
+from machine import Pin, PWM  # you may ignore the interpreter error re PWM on pycharm.
+import utime
 
-led = Pin(25, Pin.OUT)  # similar setting of pin 25 to OUTPUT
-                        # note that such a function returns an object, but also modifies this object.
-                        # this is more Object Oriented than the comparable Arduino blink native code
+# based on https://www.explainingcomputers.com/sample_code/Servo_Test.py
+# PIN SETUP:
+#   servo red should always go to 5v (servos use 4.8-6v. Note: strong servos should be powered via a dedicated power supply)
+#   servo black should go to ground
+#   servo PWM signal should be on a PWM pin, e.g. GP0 in current example
 
+# .duty_16(#) takes values of 0 to 65535 for duty cycle of 0 to 100
+# SG90 servo has 2 per cent duty cycle for 0 degrees, 12 per cent for 180.
+# So a .duty_u16 value of c.1350 is zero degrees; 8200 is 180 degrees.
 
-# another example of blinking the internal led.
-# previous code was from https://github.com/raspberrypi/pico-micropython-examples/blob/master/blink/blink.py#L1-L9
-# this version is from https://themachineshop.uk/getting-started-with-the-pi-pico-and-pycharm/
+servo = PWM(Pin(0))
+
+servo.freq(50)
 
 while True:
-    led(1)
-    time.sleep(0.5)  # this sleep code better resembles the more traditional MCU c code
-    led(0)
-    time.sleep(0.1)
+    # Move servo to zero degrees (2 per cent duty cycle)
+    servo.duty_u16(3350)  # 1350 is the minimal position
+    utime.sleep(2)
+    # Move servo to 180 degrees (12 per cent duty cycle)
+    servo.duty_u16(7200)  # is maximal position
+    utime.sleep(2)
+    print("end of cycle")
