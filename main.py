b4d1d832d42af15995a896d165b27e83eb2f7721
Guy <3strategy@gmail.com>
2021-08-18 22:36:29 +0300

    servo example. updated comments.

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ while True:
     servo.duty_u16(3350)  # 1350 is the minimal position
     utime.sleep(2)
     # Move servo to 180 degrees (12 per cent duty cycle)
-    servo.duty_u16(7200)  # is maximal position
+    servo.duty_u16(7200)  # 8200 is maximal position
     utime.sleep(2)
     print("end of cycle")
+
+# some terminal comments: you can open the teminal on pycharm using Tools>MicroPython>MicroPython REPL
+# on the terminal you can use Ctrl+c to stop the loop. Ctrl+d to reboot.
+# stopping the loop does not reset PINs
+# so servo PWN signal will continue (servo will still feel hard), and servo duty command can be sent over the REPL >>>
+# this is different from a servo that is merely connected to voltage but has no signal.
+# you can simulate that same behavior by reverting to the Blink example (whlie keeping ther servo connected).
+# or by taking out the servo singal wire (white wire).
+
